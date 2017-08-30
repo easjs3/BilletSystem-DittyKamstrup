6a83f3ff7a4ee79896cf5e6d796d2034c021edfa
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-30 11:50:30 +0200

    Opgave 1: Properties og metoder implementeret, manger tests

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        //PROPERTIES
+        public string NummerPlade { get; set; }
+        public DateTime Dato { get; set; }
+
+        //METHODS
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
