c4bb5ca3575e749be36ddac69c48076eb2392897
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-30 12:45:09 +0200

    MC klasse implementeret og tests lavet dertil

---
+++ BilletLibrary/MC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary
+{
+    public class MC
+    {
+        //PROPERTIES
+        public string NummerPlade { get; set; }
+        public DateTime Dato { get; set; }
+
+        //METHODS
+        public int Pris()
+        {
+            return 125;
+        }
+
+        public string Køretøj()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BilletLibraryTests/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PrisTestMC()
+        {
+            //Arrange
+            MC mc = new MC();
+            //Act
+            var pris = mc.Pris();
+            //Assert
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTestMC()
+        {
+            //Arrange
+            MC mc = new MC();
+            //Act
+            var køretøj = mc.Køretøj();
+            //Assert
+            Assert.AreEqual("MC", køretøj);
+        }
+    }
+}
