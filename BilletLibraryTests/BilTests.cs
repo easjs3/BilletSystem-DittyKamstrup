2fe9992bad5fcf68a80c28a060eb3757368d48a1
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-30 12:38:17 +0200

    Opgave 1: Tests implementeret

---
+++ BilletLibraryTests/BilTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTestBil()
+        {
+            //Arrange
+            Bil bil = new Bil();
+            //Act
+            var pris = bil.Pris();
+            //Assert
+            Assert.AreEqual(240, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTestBil()
+        {
+            //Arrange
+            Bil bil = new Bil();
+            //Act
+            var køretøj = bil.Køretøj();
+            //Assert
+            Assert.AreEqual("Bil", køretøj);
+        }
+    }
+}
