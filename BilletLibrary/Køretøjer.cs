d13ac8f008133f2838d76916ddc684aae94a2027
Ditte <Ditte@DESKTOP-QORFADN>
2017-09-03 16:06:36 +0200

    Opgave 5: Constructor til Køretøjer tilføjet med Exception til hvis nummerpladen har mere end 7 tegn

--- BilletLibrary/Køretøjer.cs
+++ BilletLibrary/Køretøjer.cs
@@ -12,6 +12,18 @@ namespace BilletLibrary
         public string NummerPlade { get; set; }
         public DateTime Dato { get; set; }
 
+        //CONSTRUCTOR
+        public Køretøjer(string nummerPlade, DateTime dato)
+        {
+            NummerPlade = nummerPlade;
+            Dato = dato;
+
+            if (nummerPlade.Length > 7)
+            {
+                throw new ArgumentOutOfRangeException("Nummerpladen kan maks være på 7 tegn!");
+            }
+        }
+
         //ABSTRACT METHODS
         public abstract int Pris();
         public abstract string Køretøj();
